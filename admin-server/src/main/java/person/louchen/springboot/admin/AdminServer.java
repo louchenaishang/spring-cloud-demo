27dd3a8a561b424cd785f7d418012750e712c1f0
louchenaishang <louchen@iishang.com>
2017-05-18 15:03:38 +0800

    修改日志配置

--- admin-server/src/main/java/person/louchen/springboot/admin/AdminServer.java
+++ admin-server/src/main/java/person/louchen/springboot/admin/AdminServer.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by louchen on 2017/5/18.
@@ -14,6 +16,29 @@ import org.springframework.context.annotation.Configuration;
 @EnableEurekaClient
 @EnableAdminServer
 public class AdminServer {
+
+    @Configuration
+    protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // Page with login form is served as /login.html and does a POST on /login
+            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+            // The UI does a POST on /logout on logout
+            http.logout().logoutUrl("/logout");
+            // The ui currently doesn't support csrf
+            http.csrf().disable();
+
+            // Requests for the login page and the static assets are allowed
+            http.authorizeRequests()
+                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                    .permitAll();
+            // ... and any other request needs to be authorized
+            http.authorizeRequests().antMatchers("/**").authenticated();
+
+            // Enable so that the clients can authenticate via HTTP basic for registering
+            http.httpBasic();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminServer.class, args);
